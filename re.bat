c21f3242d164469fd3ae9b81423dd274c1f8dad2
nk-world <90093664+nk-world@users.noreply.github.com>
2021-09-08 15:52:08 +0545

    Update re.bat

--- re.bat
+++ re.bat
@@ -1,15 +1,6 @@
-<<<<<<< Updated upstream
 @echo off
 timeout /t 5
 taskkill /im explorer.exe /f
 pause
 explorer
 exit
-=======
-@echo off
-timeout /t 5
-taskkill /im explorer.exe /f
-pause
-explorer
-exit
->>>>>>> Stashed changes
