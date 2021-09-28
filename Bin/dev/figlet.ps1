ba71b8b419f216350ac9627ed66a03d0351fee8a
nk-world <90093664+nk-world@users.noreply.github.com>
2021-09-28 14:49:21 +0545

    Delete Bin directory

--- Bin/dev/figlet.ps1
+++
@@ -1,3 +0,0 @@
-﻿if (!(Test-Path Variable:PSScriptRoot)) { $PSScriptRoot = Split-Path $MyInvocation.MyCommand.Path -Parent }
-$path = join-path "$psscriptroot" "..\apps\figlet\current\figlet.exe"
-if($myinvocation.expectingInput) { $input | & $path  @args } else { & $path  @args }
--- Bin/dev/smenu.vbs
+++
@@ -1,5 +0,0 @@
-Dim WSHShell
-Set WSHShell = WScript.CreateObject("WScript.Shell")
-WshShell.SendKeys "^{ESC}"
-Set WSHShell = Nothing
-WScript.Quit(0)
