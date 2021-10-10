05a63943034e9bd121d4463530bc1bc29a9f1d98
nk-world <90093664+nk-world@users.noreply.github.com>
2021-10-10 15:25:06 +0545

    Add files via upload

--- Linux/test.sh
+++ Linux/test.sh
@@ -1,1 +1,16 @@
+#!/bin/bash
 
+cat fig
+echo Linux Version
+echo ""
+echo ""
+
+while getopts k:s:l:h: flag
+do
+    case "${flag}" in
+        k) killall ${OPTARG};;
+        s) exec ${OPTARG} &;;
+        l) echo Comming soon...;;
+		h) echo "Use: process -k (process to kill) -s (start a process) -h help (display help)"
+    esac
+done
